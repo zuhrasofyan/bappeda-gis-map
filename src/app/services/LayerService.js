c788fa51853277ae615b07e6706662c2fe64f53c
Zuhra Sofyan <zuhrasofyan@gmail.com>
2018-06-22 00:36:35 +0700

    LayerService create new layer

--- src/app/services/LayerService.js
+++ src/app/services/LayerService.js
@@ -5,6 +5,23 @@ angular
 function LayerService($http, APIURL) {
   var vm = this;
 
+  function submitLayer(layer) {
+    $http.post(APIURL + 'sigap/tambah-layer/' + layer.userId, {
+      name: layer.name,
+      userId: layer.userId
+    }).then (function(result){
+      if (result.status !== 200) {
+        alert(result.data);
+      } else if (result.status === 200) {
+        alert('layer berhasil disimpan');
+        // $state.reload();
+      } else {
+        alert('Terjadi kesalahan pada server.');
+      }
+    });
+  }
+  vm.submitLayer = submitLayer;
+
   function getLayerList(id) {
     return $http.get(APIURL + 'sigap/layer-list/' + id);
   }
