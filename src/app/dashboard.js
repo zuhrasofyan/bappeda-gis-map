0c2320bc7b1407df3cbe1327e3dadd44f486ee61
Zuhra Sofyan <zuhrasofyan@gmail.com>
2018-06-22 00:37:31 +0700

    dashboard show list of layer and add layer

--- src/app/dashboard.js
+++ src/app/dashboard.js
@@ -7,15 +7,30 @@ angular
   });
 
 
-function dashboardController(UserService) {
+function dashboardController(UserService, LayerService, $state) {
   var vm = this;
 
   function getUser() {
     var a = UserService.getCurrentUser();
     return a;
   }
-
   vm.user = getUser();
+
+  LayerService.getLayerList(vm.user.id).then(function(d) {
+    vm.layerList = d.data;
+  });
+
+  vm.newLayer = {};
+
+  function addNewLayer(data) {
+    data.userId = vm.user.id;
+    LayerService.submitLayer(data);
+    LayerService.getLayerList(vm.user.id).then(function(d) {
+      vm.layerList = d.data;
+    });
+    $state.reload();
+  }
+  vm.addNewLayer = addNewLayer;
 }
 
 
